9c49c89d1ed723efc12437bedbe53b9038e45f1b
kyberdrb <kyberdrb@gmail.com>
2022-04-11 20:44:59 +0200

    Fix permissions for pacman upgrade command

--- update_arch-worker.sh
+++ update_arch-worker.sh
@@ -276,7 +276,7 @@ sudo rm -rf "$PACMAN_DB_PATH"/sync/*
 # Updating official packages
 
 terminal_emulator="$(pacman -Qq | grep terminal)"
-sudo "${terminal_emulator}" --geometry=240x24 --command="pacman --sync --refresh --refresh --sysupgrade --needed --verbose --noconfirm" 2>&1 &
+sudo "${terminal_emulator}" --geometry=240x24 --command="sudo pacman --sync --refresh --refresh --sysupgrade --needed --verbose --noconfirm" 2>&1 &
 
 
 
