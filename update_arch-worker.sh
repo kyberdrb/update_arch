9b16155656957f943bddc2c743480a0f6f558c0a
kyberdrb <kyberdrb@gmail.com>
2022-04-11 20:47:19 +0200

    Make upgrade commands blocking

--- update_arch-worker.sh
+++ update_arch-worker.sh
@@ -276,7 +276,7 @@ sudo rm -rf "$PACMAN_DB_PATH"/sync/*
 # Updating official packages
 
 terminal_emulator="$(pacman -Qq | grep terminal)"
-sudo "${terminal_emulator}" --geometry=240x24 --command="sudo pacman --sync --refresh --refresh --sysupgrade --needed --verbose --noconfirm" 2>&1 &
+sudo "${terminal_emulator}" --geometry=240x24 --command="sudo pacman --sync --refresh --refresh --sysupgrade --needed --verbose --noconfirm" 2>&1
 
 
 
@@ -311,7 +311,7 @@ done
 #   uncomment it after execution
 #   and maybe make an alias for pacman (which uncomments the line before running) and pikaur (which comments out the line before running)
 
-sudo "${terminal_emulator}" --geometry=240x24 --command="pikaur --sync --refresh --refresh --sysupgrade --verbose --noedit --nodiff --noconfirm --overwrite /usr/lib/p11-kit-trust.so --overwrite /usr/bin/fwupdate --overwrite /usr/share/man/man1/fwupdate.1.gz" 2>&1 &
+sudo "${terminal_emulator}" --geometry=240x24 --command="pikaur --sync --refresh --refresh --sysupgrade --verbose --noedit --nodiff --noconfirm --overwrite /usr/lib/p11-kit-trust.so --overwrite /usr/bin/fwupdate --overwrite /usr/share/man/man1/fwupdate.1.gz" 2>&1
 
 
 
